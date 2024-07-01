b4ba04340f0a01ebd54e5eb1619aa8d9ce5f0e6d
Sam Owens <git@sqeez.co.uk>
2024-07-01 21:42:53 +0100

    Explicit Types

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -1,19 +1,25 @@
+// Explicit Types
+var character;
+var age;
+var isLoggedIn;
 // Arrays
-var names = ['Sam', 'Willow', 'Silje'];
-names.push('Gary');
-console.log(names);
-var numbers = [10, 20, 30, 40];
-numbers.push(50);
-console.log(numbers);
-var mixedArray = ['Sam', 10, 'Willow', 20, 'Silje', 30];
-mixedArray.push('Gary', 40);
-console.log(mixedArray);
-// Objects
-var example = {
-    name: 'Sam',
-    food: 'Pizza',
-    age: 18,
-};
-example.age = 40;
-example.name = 'Harry';
+var example = [];
+example = ['Sam', 'Willow'];
+example.push('Greg');
 console.log(example);
+// Union Types
+var mixed = [];
+mixed.push('Sam');
+mixed.push(1);
+mixed.push(false);
+console.log(mixed);
+var uid;
+uid = 123;
+uid = 'XXX';
+// Objects
+var example1;
+example1 = { name: 'Sam', age: 30 };
+console.log(example1);
+var example2;
+example2 = { name: 'Sam', age: 30, height: '5ft 11' };
+console.log(example2);
--- expense-tracker/script.ts
+++ expense-tracker/script.ts
@@ -1,25 +1,35 @@
+// Explicit Types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+
 // Arrays
+let example: string[] = [];
+example = ['Sam', 'Willow'];
+example.push('Greg');
+console.log(example);
 
-let names = ['Sam', 'Willow', 'Silje'];
-names.push('Gary');
-console.log(names);
+// Union Types
+let mixed: (string | number | boolean)[] = [];
+mixed.push('Sam');
+mixed.push(1);
+mixed.push(false);
+console.log(mixed);
 
-let numbers = [10, 20, 30, 40];
-numbers.push(50);
-console.log(numbers);
-
-let mixedArray = ['Sam', 10, 'Willow', 20, 'Silje', 30];
-mixedArray.push('Gary', 40);
-console.log(mixedArray);
+let uid: string | number;
+uid = 123;
+uid = 'XXX';
 
 // Objects
+let example1: object;
+example1 = { name: 'Sam', age: 30 };
+console.log(example1);
 
-let example = {
-  name: 'Sam',
-  food: 'Pizza',
-  age: 18,
+let example2: {
+  name: string;
+  age: number;
+  height: string;
 };
 
-example.age = 40;
-example.name = 'Harry';
-console.log(example);
+example2 = { name: 'Sam', age: 30, height: '5ft 11' };
+console.log(example2);
