10c632aaa6c489be7991f06c984cdf120bd1e758
Sam Owens <git@sqeez.co.uk>
2024-07-01 22:40:04 +0100

    Function Basics

--- expense-tracker/dist/script.js
+++ expense-tracker/dist/script.js
@@ -1,5 +1,20 @@
 "use strict";
-console.log('test');
-console.log('Hello1');
-console.log('Hello2');
-console.log('test js file');
+let greet;
+greet = () => {
+    console.log('Hello World');
+};
+greet();
+// Define an arrow function named 'add' with three parameters:
+// 'a' and 'b' are numbers, and 'c' is either a number or a string with a default value of 10.
+// If we don't assign a default value to c, then it would be c? to indicate that its an optional param
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, '200');
+const minus = (a, b) => {
+    // return type in inferred by the param types
+    return a - b;
+};
+let result = minus(10, 7);
+console.log(result);
--- expense-tracker/src/script.ts
+++ expense-tracker/src/script.ts
@@ -1,3 +1,22 @@
-console.log('test');
-console.log('Hello1');
-console.log('Hello2');
+let greet: Function;
+
+greet = () => {
+  console.log('Hello World');
+};
+greet();
+
+// Define an arrow function named 'add' with three parameters:
+// 'a' and 'b' are numbers, and 'c' is either a number or a string with a default value of 10.
+// If we don't assign a default value to c, then it would be c? to indicate that its an optional param
+const add = (a: number, b: number, c: number | string = 10): void => {
+  console.log(a + b);
+  console.log(c);
+};
+add(5, 10, '200');
+
+const minus = (a: number, b: number) => {
+  // return type in inferred by the param types
+  return a - b;
+};
+let result = minus(10, 7);
+console.log(result);
