d687753665aa3698f633e6d19420ea185ca0035b
Sam Owens <git@sqeez.co.uk>
2024-07-01 21:44:50 +0100

    Additional Comments

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -1,25 +1,29 @@
-// Explicit Types
+// Declare a variable 'character' with explicit type 'string'
 var character;
+// Declare a variable 'age' with explicit type 'number'
 var age;
+// Declare a variable 'isLoggedIn' with explicit type 'boolean'
 var isLoggedIn;
-// Arrays
+// Declare a variable 'example' with explicit type of an array of strings, initialized as an empty array
 var example = [];
 example = ['Sam', 'Willow'];
 example.push('Greg');
 console.log(example);
-// Union Types
+// Declare a variable 'mixed' with explicit type of an array that can contain strings, numbers, and booleans, initialized as an empty array
 var mixed = [];
 mixed.push('Sam');
 mixed.push(1);
 mixed.push(false);
 console.log(mixed);
+// Declare a variable 'uid' with explicit union type of 'string' or 'number'
 var uid;
 uid = 123;
 uid = 'XXX';
-// Objects
+// Declare a variable 'example1' with explicit type 'object'
 var example1;
 example1 = { name: 'Sam', age: 30 };
 console.log(example1);
+// Declare a variable 'example2' with an explicit type that defines its structure with 'name' (string), 'age' (number), and 'height' (string)
 var example2;
 example2 = { name: 'Sam', age: 30, height: '5ft 11' };
 console.log(example2);
--- expense-tracker/script.ts
+++ expense-tracker/script.ts
@@ -1,30 +1,36 @@
-// Explicit Types
+// Declare a variable 'character' with explicit type 'string'
 let character: string;
+
+// Declare a variable 'age' with explicit type 'number'
 let age: number;
+
+// Declare a variable 'isLoggedIn' with explicit type 'boolean'
 let isLoggedIn: boolean;
 
-// Arrays
+// Declare a variable 'example' with explicit type of an array of strings, initialized as an empty array
 let example: string[] = [];
 example = ['Sam', 'Willow'];
 example.push('Greg');
 console.log(example);
 
-// Union Types
+// Declare a variable 'mixed' with explicit type of an array that can contain strings, numbers, and booleans, initialized as an empty array
 let mixed: (string | number | boolean)[] = [];
 mixed.push('Sam');
 mixed.push(1);
 mixed.push(false);
 console.log(mixed);
 
+// Declare a variable 'uid' with explicit union type of 'string' or 'number'
 let uid: string | number;
 uid = 123;
 uid = 'XXX';
 
-// Objects
+// Declare a variable 'example1' with explicit type 'object'
 let example1: object;
 example1 = { name: 'Sam', age: 30 };
 console.log(example1);
 
+// Declare a variable 'example2' with an explicit type that defines its structure with 'name' (string), 'age' (number), and 'height' (string)
 let example2: {
   name: string;
   age: number;
