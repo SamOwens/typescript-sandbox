4004c98f5df467bdcd948909ecd0e0d549273bbc
Sam Owens <git@sqeez.co.uk>
2024-07-01 22:05:43 +0100

    Dynamic Types

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -1,29 +1,15 @@
-// Declare a variable 'character' with explicit type 'string'
-var character;
-// Declare a variable 'age' with explicit type 'number'
-var age;
-// Declare a variable 'isLoggedIn' with explicit type 'boolean'
-var isLoggedIn;
-// Declare a variable 'example' with explicit type of an array of strings, initialized as an empty array
-var example = [];
-example = ['Sam', 'Willow'];
-example.push('Greg');
-console.log(example);
-// Declare a variable 'mixed' with explicit type of an array that can contain strings, numbers, and booleans, initialized as an empty array
+var age = 25;
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = { name: 'Sam' };
+console.log(age);
 var mixed = [];
-mixed.push('Sam');
-mixed.push(1);
-mixed.push(false);
+mixed.push(25, 'Hello', false);
 console.log(mixed);
-// Declare a variable 'uid' with explicit union type of 'string' or 'number'
-var uid;
-uid = 123;
-uid = 'XXX';
-// Declare a variable 'example1' with explicit type 'object'
-var example1;
-example1 = { name: 'Sam', age: 30 };
-console.log(example1);
-// Declare a variable 'example2' with an explicit type that defines its structure with 'name' (string), 'age' (number), and 'height' (string)
-var example2;
-example2 = { name: 'Sam', age: 30, height: '5ft 11' };
-console.log(example2);
+var example;
+example = { name: 'Sam', age: 24 };
+console.log(example);
+example = { name: 'Sam', age: '24' };
+console.log(example);
--- expense-tracker/script.ts
+++ expense-tracker/script.ts
@@ -1,41 +1,18 @@
-// Declare a variable 'character' with explicit type 'string'
-let character: string;
+let age: any = 25;
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = { name: 'Sam' };
+console.log(age);
 
-// Declare a variable 'age' with explicit type 'number'
-let age: number;
-
-// Declare a variable 'isLoggedIn' with explicit type 'boolean'
-let isLoggedIn: boolean;
-
-// Declare a variable 'example' with explicit type of an array of strings, initialized as an empty array
-let example: string[] = [];
-example = ['Sam', 'Willow'];
-example.push('Greg');
-console.log(example);
-
-// Declare a variable 'mixed' with explicit type of an array that can contain strings, numbers, and booleans, initialized as an empty array
-let mixed: (string | number | boolean)[] = [];
-mixed.push('Sam');
-mixed.push(1);
-mixed.push(false);
+let mixed: any[] = [];
+mixed.push(25, 'Hello', false);
 console.log(mixed);
 
-// Declare a variable 'uid' with explicit union type of 'string' or 'number'
-let uid: string | number;
-uid = 123;
-uid = 'XXX';
+let example: { name: any; age: any };
+example = { name: 'Sam', age: 24 };
+console.log(example);
 
-// Declare a variable 'example1' with explicit type 'object'
-let example1: object;
-example1 = { name: 'Sam', age: 30 };
-console.log(example1);
-
-// Declare a variable 'example2' with an explicit type that defines its structure with 'name' (string), 'age' (number), and 'height' (string)
-let example2: {
-  name: string;
-  age: number;
-  height: string;
-};
-
-example2 = { name: 'Sam', age: 30, height: '5ft 11' };
-console.log(example2);
+example = { name: 'Sam', age: '24' };
+console.log(example);
