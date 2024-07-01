4e336d7e42389bbd6ee1f82bdb40fc7b9f361cd8
Sam Owens <git@sqeez.co.uk>
2024-07-01 18:24:09 +0100

    Tutorial #2 - Compiling Typescript
    tsc script.ts
    
    tsc script.ts example.js
    
    tsc script.ts -w

---
+++ expense-tracker/script.js
@@ -0,0 +1,7 @@
+var firstName = 'Willow';
+console.log(firstName);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ expense-tracker/script.ts
@@ -0,0 +1,11 @@
+const firstName = 'Willow';
+
+console.log(firstName);
+
+const inputs = document.querySelectorAll('input');
+
+console.log(inputs);
+
+inputs.forEach((input) => {
+  console.log(input);
+});
